8ae51b53df4775658381bcc462dc8f4be93a7199
Deiziane <deiziane_ns@hotmail.com>
2019-05-19 14:06:19 -0300

    PUSH & COMMIT 4

--- tdd/Money.java
+++ tdd/Money.java
@@ -3,18 +3,30 @@ package tdd;
 public abstract class Money {
 	protected int amount;
 
+	private String currency;
+
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount && getClass().equals(money.getClass());
 	}
 
+	abstract Money times(int multiplier);
+
+	String currency() {
+		return currency;
+	}
+
 	static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 
 	static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
 	}
 
-	abstract Money times(int multiplier);
 }
