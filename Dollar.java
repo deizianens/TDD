96d4f02dcfaa69e5c814a52a1d53e10719c87623
Deiziane <deiziane_ns@hotmail.com>
2019-05-19 13:30:47 -0300

    PUSH & COMMIT 1

--- Dollar.java
+++ Dollar.java
@@ -1,11 +1,17 @@
 class Dollar {
-    int amount = 10;
-    Dollar(int amount) {}			
-    void times(int multiplier) {}
+    int amount;
+    Dollar(int amount) {
+        this.amount= amount;
+    }
+    void times(int multiplier) {
+        amount= amount * multiplier;
+    }
  }	
 
 public void testMultiplication() {
     Dollar five = new Dollar(5);
-    five.times(2);
-    assertEquals(10, five.amount);
+    Dollar product = five.times(2);
+    assertEquals(10, product.amount);
+    product = five.times(3);
+    assertEquals(new Dollar(15), product);
  }
